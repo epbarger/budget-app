a0a9091cf431b6dd26d0d2ce57fd33e89ed87ec6
Evan <evan@adharmonics.com>
2016-07-21 19:45:55 -0400

    fix js bug

--- app/assets/javascripts/budget.js
+++ app/assets/javascripts/budget.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('tr.click-to-expand').on('click', function(){
-    $('div.detail-row-div').slideUp(200);
     $detailRow = $(this).next('tr').find('div.detail-row-div')
+    $('div.detail-row-div').not($detailRow).slideUp(200);
     $detailRow.slideToggle(200);
   })
 })
