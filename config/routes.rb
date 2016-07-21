75ca708eacc630551e414010078477c962e1abb1
Evan <evan@adharmonics.com>
2016-07-21 00:31:19 -0400

    fix new transaction grid bullshit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'budgets#index'
+
   resources :budgets do
     resources :budget_cycles do 
       resources :balance_events, only: [:create, :show]
