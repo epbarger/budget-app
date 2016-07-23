a75acd83c1e93bea0dfd0564e6230b0a99bca3a1
Evan <evan@adharmonics.com>
2016-07-23 14:46:06 -0400

    fix more email stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'budget-app-test.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'budget-app-test.herokuapp.com' }
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
