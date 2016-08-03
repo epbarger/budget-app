b1b92ffdcd310f895e93a5567ed3dd5a34893439
Evan <evan@adharmonics.com>
2016-08-03 14:23:17 -0400

    validate new balance events only go on active cycles

--- app/models/balance_event.rb
+++ app/models/balance_event.rb
@@ -1,7 +1,15 @@
 class BalanceEvent < ApplicationRecord
   belongs_to :budget_cycle
 
+  validate :active_cycle, on: :create
+
   # budget_cycle_id
   # amount
   # note
+
+  private
+
+  def active_cycle
+    errors.add(:base, 'budget cycle is inactive') unless budget_cycle.active
+  end
 end
