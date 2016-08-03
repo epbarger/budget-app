24b05a2fa2f1762579a3ecfe96d5a188c3b4f403
Evan <evan@adharmonics.com>
2016-08-03 16:08:03 -0400

    dont show current cycle in history

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -37,7 +37,7 @@ class BudgetsController < ApplicationController
 
   def history
     @budget = current_user.account.budgets.find(params[:budget_id])
-    @budget_cycles = @budget.budget_cycles.order("end_date DESC")
+    @budget_cycles = @budget.budget_cycles.order("end_date DESC").offset(1)
   end
 
   private
